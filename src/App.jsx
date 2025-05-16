c64c6dc97fcdf5b3f8f5bae8957094dfe95856da
juancas9812 <juancas9812@gmail.com>
2025-05-15 21:34:57 -0500

    Challenge fundamentals-components (1.1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Add from "./components/Add"
-import Greet from "./components/Greet"
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
 
 const App = () => {
-  return <Add />
-  return <Greet />
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>This is the solution to exercise/challenge 1.1 (minute 24:35 of video)</p>
+    </main>
+  )
+}
+
+export default MainContent
