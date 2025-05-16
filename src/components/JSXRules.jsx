f4ff7d857fee20da0e78c5ec627ff5a3e5737563
juancas9812 <juancas9812@gmail.com>
2025-05-15 21:53:23 -0500

    challenge 1.3 fundamentals-jsx rules

---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,12 @@
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p>1: single parent element</p>
+      <p>2: all elements must be closed</p>
+      <p>3: attributes in camelCase like className</p>
+    </div>
+  )
+}
+
+export default JSXRules;
